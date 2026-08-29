1d9e59d98b5379857e2657d9ef1b2e19bc6ca2c9
samith <nmsamith@gmail.com>
2026-08-30 00:17:12 +0530

    Add initial project files including .gitignore, Cargo.toml, and main.rs

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use color_eyre::eyre::{Ok, Result};
+use ratatui::{DefaultTerminal, crossterm::event::{self, Event::{self, Key}}};
+fn main() -> Result<()>{
+
+    println!("Hello, world!");
+    color_eyre::install()?;
+    
+    let terminal = ratatui::init();
+    
+    let result= run(terminal);
+    ratatui::restore();
+    result
+    
+
+}
+fn run(mut terminal: DefaultTerminal)->Result<()>{
+    loop{
+
+        if let Event::Key(key) = event::read()?   {
+            match key.code {
+                event::KeyCode::Esc =>{
+                    break;
+                }
+                event::KeyCode::Up => todo!(),
+                event::KeyCode::Down => todo!(),
+                event::KeyCode::Left => todo!(),
+                event::KeyCode::Right => todo!(),
+                event::KeyCode::Backspace => todo!(),
+                
+                
+                
+                event::KeyCode::Tab => todo!(),
+                event::KeyCode::Delete => todo!(),
+                _ =>{ },
+                
+              
+               
+            }
+            
+        }
+    }
+
+    Ok(())
+}
